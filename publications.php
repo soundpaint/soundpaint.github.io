0e2cba1906aaaff8c6a6eb66c9e77f659a63428e
Jürgen Reuter <juergen@soundpaint.org>
2025-10-04 07:16:46 +0200

    Redirect for Broken Link
    * _config.yml, publications.php:
      For backwards compatibility with
      (now) broken links published in
      old papers, re-add a new file
      "publications.php" that redirects
      to the actual present list of
      publications (English version).

---
+++ publications.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <script>
+      document.ready(window.location.href = 'en/publications.html');
+    </script>
+  </head>
+  <body>
+    <p>
+      This page has moved.  Please click
+      <a href="en/publications.html">here</a>
+      to go to my list of publications.  --jr
+    </p>
+  </body>
+</html>
